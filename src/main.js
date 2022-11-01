6ba68da0a6c16b92632e5cafde2e9d2852811db1
Siddharthan Dileep <79396917+vaasew@users.noreply.github.com>
2022-11-01 23:48:03 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import './style.css'
-//page reload at port 3000 due to "start" in package.json
+import './style.css';
+const APP_URL = import.meta.env.VITE_APP_URL;
 
-document.addEventListener("DOMContentLoaded",()=>{
-  if(localStorage.getItem("accessToken")){
-    window.location.href=`${APP_URL}/dashboard/dashboard.html`;
-  }
-  else{
-    window.location.href=`${APP_URL}/login/login.html`
-    // window.location.href = 'http://www.google.com'; //Will take you to Google.
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("accessToken")) {
+    window.location.href = `${APP_URL}/dashboard/dashboard.html`;
+  } else {
+    window.location.href = `${APP_URL}/login/login.html`;
   }
 })
+//page reload at port 3000 due to "start" in package.json
+
+
+    // window.location.href = 'http://www.google.com'; //Will take you to Google.
+
