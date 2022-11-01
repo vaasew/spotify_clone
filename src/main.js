134216e9742cd7bd435534974112be9c7d6c68b6
Siddharthan Dileep <siddharthandileep@gmail.com>
2022-11-02 00:25:42 +0530

    bug fixes

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-import './style.css'
+
 //page reload at port 3000 due to "start" in package.json
 
-document.addEventListener("DOMContentLoaded",()=>{
-  if(localStorage.getItem("accessToken")){
-    window.location.href=`${APP_URL}/dashboard/dashboard.html`;
-  }
-  else{
-    window.location.href=`${APP_URL}/login/login.html`
-    // window.location.href = 'http://www.google.com'; //Will take you to Google.
+import './style.css';
+const APP_URL = import.meta.env.VITE_APP_URL;
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("accessToken")) {
+    window.location.href = `${APP_URL}/dashboard/dashboard.html`;
+  } else {
+    window.location.href = `${APP_URL}/login/login.html`;
   }
 })
