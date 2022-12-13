63a8c76c4023310f208786f041b249abd70bb00a
Siddharthan Dileep <79396917+vaasew@users.noreply.github.com>
2022-12-13 14:56:29 +0530

    Update common.js

--- src/common.js
+++ src/common.js
@@ -1,7 +1,7 @@
 export const ACCESS_TOKEN="ACCESS_TOKEN";
 export const TOKEN_TYPE="TOKEN_TYPE";
 export const EXPIRES_IN="EXPIRES_IN";
-const APP_URL=import.meta.env.VITE_APP_URL;
+export const APP_URL=import.meta.env.VITE_APP_URL;
 
 export const ENDPOINT={
     userInfo:"me",
